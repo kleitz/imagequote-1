10398563358773b664537ee76837ac49f18cc47e
monext <mo_next@mail.ru>
2014-07-29 01:47:11 +0300

    added User class, updated config, created connection to MySQL DB

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 module.exports = {
-	"PORT": 8080,
+	"PORT": 80,
 	"WEB_APP": "/web-app",
 	"MODULES": "/modules",
-	"DB_USER": "imagequote",
-	"DB_PASSWORD": "iqtwnbre22"
+    "DB_HOST": "localhost",
+    "DB_USER": "imagequote",
+	"DB_PASSWORD": "iqtwnbre22",
+	"DB_NAME": "imagequote"
 };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var PROJECT_PATH = process.cwd(),
     CONFIG = require(PROJECT_PATH + "/config.js"),
-    App = require(PROJECT_PATH + "/modules/imagequote.core.node.js"),
+    App = require(PROJECT_PATH + "/modules/imagequote.Core.node.js"),
     http = require("http"),
     app = new App({
         "PROJECT_PATH": PROJECT_PATH,
