e51abe9ff3efe469dc7120a519e1e3b929636ee0
monext <mo_next@mail.ru>
2014-08-01 20:43:58 +0300

    copied index.js into server.js, changed config

--- config.js
+++ config.js
@@ -1,19 +1,12 @@
 var CONFIG = {
-    "PORT": "8080",
-    "IP": "127.0.0.1",
+    "PORT": process.env.OPENSHIFT_NODEJS_PORT || "8080",
+    "IP": process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1",
     "WEB_APP": "/web-app",
     "MODULES": "/modules",
-    "DB_HOST": "localhost",
+    "DB_HOST": process.env.OPENSHIFT_MYSQL_DB_HOST || "localhost",
     "DB_USER": "imagequote",
     "DB_PASSWORD": "iqtwnbre22",
     "DB_NAME": "imagequote"
 };
-
-//if running on Openshift server
-if (process.env.OPENSHIFT_NODEJS_PORT) {
-    CONFIG.PORT = process.env.OPENSHIFT_NODEJS_PORT;
-    CONFIG.IP = process.env.OPENSHIFT_NODEJS_IP;
-    CONFIG.DB_HOST = process.env.OPENSHIFT_MYSQL_DB_HOST;
-}
 
 module.exports = CONFIG;
--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log(process.env);
+var PROJECT_PATH = process.cwd(),
+    CONFIG = require(PROJECT_PATH + "/config.js"),
+    App = require(PROJECT_PATH + "/modules/imagequote.Core.node.js"),
+    http = require("http"),
+    app = new App({
+        "PROJECT_PATH": PROJECT_PATH,
+        "CONFIG": CONFIG
+    });
+
+http.createServer(function (request, response) {
+    app.onRequest(this, request, response);
+}).listen(CONFIG.PORT, CONFIG.IP);
+
+console.log("Server Running on port " + CONFIG.PORT);
