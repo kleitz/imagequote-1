0076773866288e9c61b43089bdb909db169de7ff
Andrii <mo_next@mail.ru>
2014-08-07 06:49:55 +0300

    Test commit

--- server.js
+++ server.js
@@ -11,5 +11,4 @@ http.createServer(function (request, response) {
     app.onRequest(this, request, response);
 }).listen(CONFIG.PORT, CONFIG.IP);
 
-
 console.log("Server Running on port " + CONFIG.PORT);
